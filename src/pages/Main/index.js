aa43a1695b5b2c96d46eb52c807359ab28d37895
Léu Almeida <leo@webid.net.br>
2019-12-14 20:11:56 -0300

    feat: Incrementing initial navigation to Main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 // import { Container } from './styles';
 
-export default function Main() {
-  return <View />;
+class Main extends Component {
+  state = {};
+
+  handleChangePage = item => {
+    const { navigation } = this.props;
+
+    navigation.navigate('Main', { item });
+  };
+
+  render() {
+    return (
+      <View style={{ height: 400, backgroundColor: 'red' }}>
+        <TouchableOpacity
+          style={{ height: 400, backgroundColor: 'red' }}
+          onPress={() => this.props.navigation.navigate('Cart')}
+        >
+          <Text>Ir para a página de carrinho</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
+
+export default Main;
