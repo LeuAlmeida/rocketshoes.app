135329599b1b10926a0181d266f1c1e40400e9f6
Léu Almeida <leo@webid.net.br>
2019-12-14 20:11:49 -0300

    feat: Incrementing initial navigation to Cart page

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 // import { Container } from './styles';
 
-export default function Cart() {
-  return <View />;
+class Cart extends Component {
+  state = {};
+
+  render() {
+    return (
+      <View style={{ height: 400, backgroundColor: 'red' }}>
+        <TouchableOpacity
+          style={{ height: 400, backgroundColor: 'red' }}
+          onPress={() => this.props.navigation.navigate('Main')}
+        >
+          <Text>Ir para a página inicial</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
+
+export default Cart;
