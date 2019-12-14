89362f07570d469775f0856c8738911e1eda3070
Léu Almeida <leo@webid.net.br>
2019-12-14 19:56:27 -0300

    chore: Incrementing the index file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StatusBar } from 'react-native';
 
-// import { Container } from './styles';
+import Routes from './routes';
 
 export default function App() {
-  return <View />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#333" />
+      <Routes />
+    </>
+  );
 }
