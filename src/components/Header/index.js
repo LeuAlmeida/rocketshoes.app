2a8181df52685783e70a90db316f0f61a0ca1285
LeuAlmeida <leo@webid.net.br>
2020-01-08 19:36:32 -0300

    feat: Create logo container to improve a navigation to Main page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,13 +3,22 @@ import React from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Wrapper, Container, Logo, BasketContainer, ItemCount } from './styles';
+import {
+  Wrapper,
+  Container,
+  LogoContainer,
+  Logo,
+  BasketContainer,
+  ItemCount,
+} from './styles';
 
 function Header({ navigation, cartSize }) {
   return (
     <Wrapper>
       <Container>
-        <Logo />
+        <LogoContainer onPress={() => navigation.navigate('Main')}>
+          <Logo />
+        </LogoContainer>
         <BasketContainer onPress={() => navigation.navigate('Cart')}>
           <Icon name="shopping-basket" color="#FFF" size={24} />
           <ItemCount>{cartSize || 0}</ItemCount>
