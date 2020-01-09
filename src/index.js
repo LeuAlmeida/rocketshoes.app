6e19fc999df35f9cafa8ac5ea402f5991255466e
LeuAlmeida <leo@webid.net.br>
2020-01-09 14:08:24 -0300

    chore: Improving store redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
+
 import './config/ReactotronConfig';
+
+import store from './store';
 
 import Routes from './routes';
 
 export default function App() {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar barStyle="light-content" backgroundColor="#333" />
       <Routes />
-    </>
+    </Provider>
   );
 }
