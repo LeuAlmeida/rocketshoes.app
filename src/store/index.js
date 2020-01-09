0b70368a5d278bfa1e79d422cab0b7ae6995c76a
LeuAlmeida <leo@webid.net.br>
2020-01-09 14:12:42 -0300

    chore: Incrementing root reducer to index redux store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore } from 'redux';
 
-import reducer from './modules/cart/reducer';
+import rootReducer from './modules/rootReducer';
 
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 export default store;
