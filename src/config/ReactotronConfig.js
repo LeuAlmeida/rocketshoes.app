c6cddb043b71f63b15713375da2c291831955ba0
LeuAlmeida <leo@webid.net.br>
2020-01-09 16:37:34 -0300

    chore: Use reactotron redux

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,11 +1,13 @@
 import Reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
 
 if (__DEV__) {
   const tron = Reactotron.configure({ host: '127.0.0.1' })
+    .use(reactotronRedux())
     .useReactNative()
     .connect();
 
-  console.tron = tron;
-
   tron.clear();
+
+  console.tron = tron;
 }
