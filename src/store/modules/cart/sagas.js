d9736b861e795982398e16f036935acfead6e412
Léu Almeida <leo@webid.net.br>
2020-01-10 22:54:06 -0300

    chore: Improvements of addToCart response

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,7 +1,13 @@
-import { call } from 'redux-saga/effects';
+import { call, put, all, takeLatest } from 'redux-saga/effects';
 
 import api from '../../../services/api';
 
+import { addToCartSuccess } from './actions';
+
 function* addToCart({ id }) {
   const response = yield call(api.get, `/products/${id}`);
+
+  yield put(addToCartSuccess(response.data));
 }
+
+export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
