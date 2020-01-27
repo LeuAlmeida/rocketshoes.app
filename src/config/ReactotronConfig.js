380e0a6f33db167686d98a3ce4afb0e485f574cf
LeuAlmeida <leo@webid.net.br>
2020-01-27 13:19:10 -0300

    feat: Improving Reactotron with Saga connection

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,9 +1,11 @@
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
+import reactotronReduxSaga from 'reactotron-redux-saga';
 
 if (__DEV__) {
   const tron = Reactotron.configure({ host: 'localhost' })
     .use(reactotronRedux())
+    .use(reactotronReduxSaga())
     .useReactNative()
     .connect();
 
